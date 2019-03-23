479922edfab7b4e2eb799a8353bdf4c7ec562072
David Markham <davidmarkham76@gmail.com>
2019-03-23 16:11:17 +0000

    Started solution number 7

---
+++ solution-7.py
@@ -0,0 +1,7 @@
+# David Markham 2019-03-23
+# Problem number 7 
+
+# Write a program that that takes a positive floating point number as input
+# And outputs an approximation of its square root.
+
+# Loops week 4
