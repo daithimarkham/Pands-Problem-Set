b355f1c6f7d6c702b08eb563265d5f6d6c4cccb3
David Markham <davidmarkham76@gmail.com>
2019-03-26 17:45:48 +0000

    Finished solution 9

--- solution-9.py
+++ solution-9.py
@@ -4,21 +4,25 @@
 # Write a program that reads in a text file and outputs every second line. 
 # The program should take the filename from an argument on the command line.
 
-f = open('mobydick.txt', 'r')
 
-s = f.read()
+# Write a statement to get the user to enter the file they wish to use. 
+user = input("Please open file: ")
 
-print(s)
-with open('mobydick.txt', 'r') as f:
+# I was having trouble finding the help on the Python tutorial so I went to Stack Overflow for help;
+# https://stackoverflow.com/questions/30551945/how-do-i-get-python-to-read-only-every-other-line-from-a-file-that-contains-a-po
+# This prints every even line, which is the equivalent of every second line of the user input. 
+# I created a text file: mobydick.txt to ensure it was correct which it was. 
+with open(user, 'r') as f:
     count = 0
     for line in f:
         count+=1
         if count % 2 == 0:
             print(line) 
-
+# f.close() is good practice to end the program when you've finished. 
 f.close()
 
 
 
 
-# https://stackoverflow.com/questions/30551945/how-do-i-get-python-to-read-only-every-other-line-from-a-file-that-contains-a-po
+
+# 
