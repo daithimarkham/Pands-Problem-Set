3b999447e9af0725d9dfe9000e9c4d43a228481b
David Markham <davidmarkham76@gmail.com>
2019-03-27 09:09:55 +0000

    Finished solution 9

--- solution-9.py
+++ solution-9.py
@@ -4,9 +4,11 @@
 # Write a program that reads in a text file and outputs every second line. 
 # The program should take the filename from an argument on the command line.
 
-
+# This is extra pieces of information that the user provided to your Python script.
+# Foe eg, what the text file is called that the user wants to use.
+import sys
 # Write a statement to get the user to enter the file they wish to use. 
-user = input("Please open file: ")
+user = input("Please enter file name: ")
 
 # I was having trouble finding the help on the Python tutorial so I went to Stack Overflow for help;
 # https://stackoverflow.com/questions/30551945/how-do-i-get-python-to-read-only-every-other-line-from-a-file-that-contains-a-po
